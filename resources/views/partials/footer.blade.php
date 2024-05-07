498085352cb0f851a6d3d4f69e5b6f633eaf5063
mattemutti <mattemutti@gmail.com>
2024-05-07 21:53:56 +0200

    add footer bg

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,15 @@
 <footer>
-    Footer
+
+    <div class="img_footer">
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    ciao
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 </footer>

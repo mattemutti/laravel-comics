7305ba2015f1cc66afa63aa4fc228d0dae516e12
mattemutti <mattemutti@gmail.com>
2024-05-07 18:05:56 +0200

    add card dinamics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,18 +4,22 @@
     <div class="container">
 
         <div class="row">
-
             <div class="title mb-5">
                 CURRENT SERIES
             </div>
 
         </div>
         <div class="row">
-
-
-            <div class="col">
-                ciao bastardu
-            </div>
+            @foreach ($comics as $comic)
+                <div class="col-2">
+                    <div class="card bg_card">
+                        <img src="{{ $comic['thumb'] }}" alt="">
+                        <h6 class="title_card text-uppercase py-4">{{ $comic['series'] }}</h6>
+                    </div>
+                </div>
+            @endforeach
         </div>
+
+        <button>LOAD MORE</button>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('comics');
+
+    $comics = config('db.comics');
+    //dd($comics);
+
+    return view('comics', compact('comics'));
 })->name('comics');
 
 Route::get('/info', function () {

23803d8be83c645ef6579d00c5a0767c08c4b2db
mattemutti <mattemutti@gmail.com>
2024-05-07 22:24:13 +0200

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,81 @@
 <footer>
 
     <div class="img_footer">
-
-        <div class="container">
+        <div class="container p-5">
             <div class="row">
-                <div class="col">
-                    ciao
+                <div class="col-2 ">
+                    <ul class="list_footer">
+                        <li class="color_title">DC COMICS</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                    </ul>
+                    <ul class="list_footer">
+                        <li class="color_title">SHOP</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                    </ul>
+
+                </div>
+                <div class="col-2 ">
+                    <ul class="list_footer">
+                        <li class="color_title">DC</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                    </ul>
+                </div>
+                <div class="col-2 ">
+                    <ul class="list_footer">
+                        <li class="color_title">SITE</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                        <li>Caracters</li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
+    {{-- footer top --}}
+
+
+    <div class="container footer_bottom align-content-center p-0">
+        <div class="row justify-content-between align-items-center">
+            <div class="col ">
+                <button>SING-UP NOW</button>
+            </div>
+            <div class="col d-flex justify-content-end align-items-center">
+
+                <div class="color_bottom">FOLLOW US</div>
+                <a href=""><img class="px-2" src="{{ Vite::asset('resources/img/footer-facebook.png') }}"
+                        alt=""></a>
+                <a href=""><img class="px-2" src="{{ Vite::asset('resources/img/footer-periscope.png') }}"
+                        alt=""></a>
+                <a href=""><img class="px-2" src="{{ Vite::asset('resources/img/footer-pinterest.png') }}"
+                        alt=""></a>
+                <a href=""><img class="px-2" src="{{ Vite::asset('resources/img/footer-twitter.png') }}"
+                        alt=""></a>
+                <a href=""><img class="px-2" src="{{ Vite::asset('resources/img/footer-youtube.png') }}"
+                        alt=""></a>
+
+
+            </div>
+        </div>
+    </div>
+
+
 
 
 </footer>

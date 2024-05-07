068f8c526803480611d95962f376f54f956a42aa
mattemutti <mattemutti@gmail.com>
2024-05-07 16:09:57 +0200

    add top header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,21 @@
 <header>
 
-    Header
+    <div class="top_header">
+        <div class="container">
+            <div class="row justify-content-end text-end g-0">
+                <div class="col-3"><a class="text-white text-decoration-none" href="">DC POWER VISA</a></div>
+                <div class="col-3"><a class="text-white text-decoration-none" href="">ADDITIONAL DC SITES</a></div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container nav_header">
+
+
+
+
+
+    </div>
 
 
 </header>

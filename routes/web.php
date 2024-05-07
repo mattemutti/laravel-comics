107b76a1800fe8f1405958640796c91dfefc524a
mattemutti <mattemutti@gmail.com>
2024-05-07 15:51:52 +0200

    add partials and route page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('comics');
+})->name('comics');
+
+Route::get('/info', function () {
+    return view('info');
+})->name('info');

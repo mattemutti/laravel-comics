081854f554ebf41d93eb50e8a215a5857c8463fa
mattemutti <mattemutti@gmail.com>
2024-05-08 18:19:40 +0200

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -10,42 +10,40 @@
                     <ul class="list_footer">
                         <li class="color_title">DC COMICS</li>
                         <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
+                        <li>Comics</li>
+                        <li>Movie</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
                     </ul>
                     <ul class="list_footer">
                         <li class="color_title">SHOP</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
                     </ul>
 
                 </div>
                 <div class="col-2 ">
                     <ul class="list_footer">
                         <li class="color_title">DC</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
+                        <li>Therms Of Use</li>
+                        <li>Privact Policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>TalemWorkshop</li>
                     </ul>
                 </div>
                 <div class="col-2 ">
                     <ul class="list_footer">
                         <li class="color_title">SITE</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
-                        <li>Caracters</li>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
                     </ul>
                 </div>
             </div>
@@ -57,7 +55,7 @@
     <div class="container footer_bottom align-content-center p-0">
         <div class="row justify-content-between align-items-center">
             <div class="col ">
-                <button>SING-UP NOW</button>
+                <button class="btn btn-outline-primary rounded-0 text-white">SING-UP NOW!</button>
             </div>
             <div class="col d-flex justify-content-end align-items-center">
 
